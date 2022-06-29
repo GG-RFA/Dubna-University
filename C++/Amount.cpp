6144bce4db7eab6389adb6c6cce767d32768dd02
Fedor Ronginskiy <108332251+TeddyRFA@users.noreply.github.com>
2022-06-29 11:37:48 +0300

    Add files via upload

---
+++ C++/Amount.cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+class A {
+protected:
+	double a;
+	string name;
+public:
+	A(double a, string name) {
+		this->a = a;
+	}
+	virtual void show() {
+		cout « "x=" « a «"\t y="«"\t Объем фигуры(1-куб,2-циллиндр,3-окружность,4-пирамида)="«Vol()« "\n";
+	}
+	virtual double Vol() = 0;
+};
+class Coub :public A {
+public:
+	Coub(double a, string name) :A(a, name) {}
+	double Vol() {
+		return pow(a, 3);
+	}
+};
+class Cylinder : public A {
+public:
+	Cylinder(double a, string name) :A(a, name) {}
+	double Vol() {
+		return 3.14 * pow(a, 3);
+	}
+};
+class Ball : public A {
+public:
+	Ball(double a, string name) :A(a, name) {}
+	double Vol() {
+		return 4 / 3 * 3.14 * pow(a, 2);
+	}
+};
+class Pyramide :public A {
+public:
+	Pyramide(double a, string name) :A(a, name) {}
+	double Vol() {
+		return (a / 2) * (a / sqrt(2));
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Coub coub(3, "Куб");
+	coub.show();
+	Cylinder cylinder(3, "Циллинр");
+	cylinder.show();
+	Ball ball(3, "Шар");
+	ball.show();
+	Pyramide pyramide(3, "Пирамида");
+	pyramide.show();
+	system("pause");
+	return 0;
+}
---
+++ C++/Array.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class A {
+private:
+
+};
+int main() {
+	setlocale(LC_ALL, "Russian");
+	char a[] = "Здравствуй,мир";
+	char b[] = { 'П','р','и','в','е','т','\0' };
+	cout « a « endl;
+	for (int i = 0; i < sizeof(a) / sizeof(char); i++)
+		cout « a[i];
+	cout « endl;
+	for (int i = 0; a[i] != '\0'; i++)
+		cout « a[i];
+	cout « endl;
+	cout « b « endl;
+	system("pause");
+	return 0;
+}
