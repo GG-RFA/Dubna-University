261ff6ff4e04068d3205ff8e2c51f1cace5a9a67
Fedor Ronginskiy <108332251+TeddyRFA@users.noreply.github.com>
2022-06-29 19:10:33 +0300

    Add files via upload

---
+++ C++/Task_04.10.2021.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int gauss1(double* a, double* b, int n)//прямой ход 
+{
+    int kk = 0; // kk - номер текущего диагонального элемента
+    int n1 = n + 1; // n1 - расстояние между двумя соседними даигональными элементами
+    int kn = n; // kn - номер первого элемента следующей обрабатываемой строки
+    double s;
+    int k, ik, ij, kj, i; // k - текущий номер обрабатываемой строки
+    for (k = 0; k < n; k++) 
+    {
+        for (ik = kk + n, i = k + 1; i < n; ik += n, i++) 
+        {
+            s = a[ik] / a[kk];
+            for (ij = ik + 1, kj = kk + 1; kj < kn; ij++, kj++) 
+            {
+                a[ij] -= s * a[kj];
+                b[i] -= s * b[k];
+            }
+        }
+        kk += n1;
+        kn += n;
+    }
+    return 0;
+}
+
+int gauss2(double* a, double* b, int n) // обратный ход
+{ 
+    int kk = n * n - 1; // kk - номер диагонального элемента a[kk]
+    int n1 = n + 1; // n1 - шаг диагонали
+    double s;
+    int k, kj, j;
+    // Вектор решения накладывается на столбец свободных членов
+    for (k = n - 1; k >= 0; k--) 
+    {
+        s = 0;
+        for (kj = kk + 1, j = k + 1; j < n; kj++, j++)
+            s += a[kj] * b[j];
+        b[k] = (b[k] - s) / a[kk];
+        kk -= n1;
+    }
+    return 0;
+}
+
+int main()
+{
+    double a[] = { 2, 3, 4, 5, 0, 6, 7, 8, 0, 0, 9, 10, 0, 0, 0, 11 };
+    double b[] = { 40, 65, 67, 44 };
+    const int n = 4;
+    cout << gauss2(a, b, n) << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << b[i] << "\t";
+        cout << "\n";
+    }
+}
+
+
---
+++ C++/Tasks_27.09.2021.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+#include <thread>
+using namespace std;
+
+class A {
+public:
+    void operator()(int a[],int len) 
+    {
+        for (int i = 0; i < len; i++) 
+        {
+            for (int i = 0; i < 1000000; i++);
+            a[i] *= 2;
+        }
+    }
+    void B(int a[], int len) {
+        for (int i = 0; i < len; i++) 
+        {
+            for (int i = 0; i < 1000000; i++);
+            a[i] = 9;
+        }
+    }
+};
+int main()
+{
+    const int length = 5;
+    int a[length] = { 1,2,3,4,5 };
+    A obj;
+    thread a_thread(obj, a, length);
+    thread b_thread(&A::B, &obj, a, length);
+    if (a_thread.joinable()) {
+        a_thread.join();
+    }
+    if (b_thread.joinable()) {
+        b_thread.join();
+    }
+    for (int i = 0; i < length; i++) 
+    {
+        cout << a[i] << "\t";
+    }
+    
+    return 0;
+}
