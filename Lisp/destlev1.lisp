bcdf29b96bc367ac5d982aec85de247e3ff1711c
Fedor Ronginskiy <108332251+TeddyRFA@users.noreply.github.com>
2022-06-29 18:51:06 +0300

    Delete destlev1.lisp

--- Lisp/destlev1.lisp
+++
@@ -1,5 +0,0 @@
-(defun destlev1 (List)
-	   (cond
-	     ((null List) nil)
-	     ((atom (car List)) (cons (car List) (destlev1 (cdr List))))
-	     (t (append (destlev1 (car List)) (destlev1 (cdr List))))))
