b6dba244b3f0710c0dca52eb9d4d77e5f2d95346
Fedor Ronginskiy <108332251+TeddyRFA@users.noreply.github.com>
2022-06-29 12:03:57 +0300

    Add files via upload

---
+++ C++/Production.cpp
@@ -0,0 +1,61 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+void ShellSort(T data[], int n) {
+	int i = 0;
+	T key;
+	int step = n / 2;
+	while (step > 0) {
+		for (int j = step; j < n; j++) {
+			key = data[j];
+			for (i = j; i > 0; i -= step) {
+				if (data[i - step] < key) {
+					break;
+				}
+				data[i] = data[i - step];
+			}
+			data[i] = key;
+		}
+		/*for (int k = 0; k < n; k++);
+			cout << data[k] << ' ';
+			cout << '\n';
+			step /= 2;*/
+	}
+}
+
+class parts {
+private:
+	string vendor_code;
+	string part_name;
+	string manufacturer;
+	int price;
+public:
+	parts() : vendor_code(""), part_name(""), manufacturer(""), price(0) {};
+	parts(string vendor_code, string part_name, string manufacturer, int price) : vendor_code(vendor_code), part_name(part_name), manufacturer(manufacturer), price(price) {};
+	string toString() {
+		return vendor_code + '\t' + part_name + '\t' + manufacturer + '\t' + to_string(price) + '\n';
+	}
+	int operator <(parts& st) {
+		return (manufacturer < st.manufacturer) || (manufacturer == manufacturer) && (vendor_code < vendor_code);
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 7;
+	parts A[n] = { parts("21093-8402010-00", "Капот", "АО «АвтоВАЗ»", 1900),
+	parts("21140-8402910-00 ", "Капот", "АО «АвтоВАЗ»", 3900),
+	parts("21140-8403010-01", "Крыло переднее правое", "ПАО «КАМАЗ» ", 1200),
+	parts("21083-6816083", "Ручка подлокотника левая", "АО «АвтоВАЗ»",80),
+	parts("21083-1003011 ", "Головка блока в сборе", "АО «АвтоВАЗ»",16580),
+	parts("406-1006262", "Фланец промежуточного вала", "ОАО «ГАЗ» ",175),
+	parts("21140-3711010-00","Фара правая","ЗАО «АВТОСВЕТ»",1700) };
+	ShellSort(A, n);
+	for (int i = 0; i < n; i++)
+	cout << A[i].toString();
+	cout << '\n';
+	return 0;
+}
---
+++ C++/School.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+void insertSort(T data[], int n) {
+	int i = 0;
+	T key;
+	for (int j = 1; j < n; j++) {
+		key = data[j];
+		for (i = j; i > 0; i--) {
+			if (data[i - 1] < key) {
+				break;
+			}
+			data[i] = data[i - 1];
+		}
+		data[i] = key;
+	}
+}
+class pupil {
+private:
+	string num_class;
+	int num_magazine;
+	string FIO;
+	string sex;
+public:
+	pupil() : num_class(""), num_magazine(0), FIO(""), sex("") {};
+	pupil(string num_class, int num_magazine, string FIO, string sex) : num_class(num_class), num_magazine(num_magazine), FIO(FIO), sex(sex) {};
+	string toString() {
+		return num_class + '\t' + to_string(num_magazine) + '\t' + FIO + '\t' + sex + '\n';
+	}
+	int operator <(pupil& st) {
+		return (sex < st.sex) || (sex == st.sex) && (FIO < st.FIO);
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 6;
+	pupil A[n] = { pupil("10A", 1, "Иванов Сергей Петрович","М"),
+	pupil("10A", 2, "Костин Петр Васильевич","М"),
+	pupil("10A", 3, "Матросова Елена Ивановна","Ж"),
+	pupil("11Б", 1, "Богданов Юрий Сергеевич","М"),
+	pupil("11Б", 2, "Потапова Юлия Петровна","Ж"),
+	pupil("11Б", 3, "Сорокина Ольга Петровна","Ж") };
+	insertSort(A, n);
+	for (int i = 0; i < n; i++)
+		cout << A[i].toString();
+	cout << '\n';
+	return 0;
+}
