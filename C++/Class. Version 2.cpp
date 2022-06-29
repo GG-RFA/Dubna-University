9a3ef47cc23f922353c80f85c16f2e8685ef793c
Fedor Ronginskiy <108332251+TeddyRFA@users.noreply.github.com>
2022-06-29 11:41:26 +0300

    Add files via upload

---
+++ C++/Class. Version 2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class A {
+public:
+	int n;
+	int* a;
+	A(int n, int start) {
+		this->n = n;
+		a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = start++;
+	}
+	~A() { delete[] a; }
+	int& operator[](int i) {
+		return a[i];
+	}
+};
+int main() {
+	setlocale(LC_ALL, "Russian");
+	A b(3, 10);
+	for (int i = 0; i < b.n; i++)
+		cout « b[i] « '\t';
+	cout « endl;
+	system("pause");
+	return 0;
+}
---
+++ C++/Class.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+class A {
+private:
+	int size;
+	int* nums;
+public:
+	A(int size, int* nums) {
+		this->size = size;
+		this->nums = new int[size];
+		for (int i = 0; i < size; i++)
+			this->nums[i] = nums[i];
+	}
+	void show() {
+		for (int i = 0; i < size; i++)
+			cout « nums[i] « '\t';
+		cout « '\n';
+	}
+	~A() {
+		delete[]nums;
+		cout « "**\n";
+	}
+	A(A& a) {
+		size = a.size;
+		nums = new int[size];
+		for (int i = 0; i < size; i++)
+			nums[i] = a.nums[i];
+	}
+	A& operator=(A& a) {
+		delete[] nums;
+		size = a.size;
+		nums = new int[size];
+		for (int i = 0; i < size; i++)
+			nums[i] = a.nums[i];
+		return *this;
+	}
+};
+int main()
+{
+	int a[] = { 3,4,5,6 };
+	A b(4, a);
+	b.show();
+	int c[] = { 10,15,20 };
+	A d(3, c);
+	d.show();
+	A e(b);
+	e = b = d;
+	e.show();
+	setlocale(LC_ALL, "Russian");
+	system("pause");
+	return 0;
+}
