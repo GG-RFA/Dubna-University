f86bfea58954a208e5e251018c7518258ed18b1b
Fedor Ronginskiy <108332251+TeddyRFA@users.noreply.github.com>
2022-06-29 11:38:33 +0300

    Add files via upload

---
+++ C++/Object_Close.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+class A
+{
+public: string name;
+	  A(string name) { this->name = name; }
+	  //A() { name = "***"; }
+	  ~A() { cout « "Закрыть объект" « "\n"; }
+	  void show() { cout « name « "\n"; }
+};
+int main() {
+	setlocale(0, "");
+	A a("A"), b("B");
+	A* c = new A("C");
+	a.show(); b.show(); c->show();
+	delete c;
+	return 0;
+}
---
+++ C++/Object_Delete.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+class A {
+private:
+	long long n;
+public:
+	void set(long long n) { this->n = n; }
+	long long get() { return n; }
+	~A() { cout « "Удаляем объект с полем:" « n « endl; }
+};
+int main() {
+	setlocale(LC_ALL, "Russian");
+	const int n = 10;
+	A a[10];
+	A* start, * finish;
+	start = a;
+	finish = start + n;
+	for (int i = 0; i < n; i++)
+		a[i].set(i);
+	while (start != finish) {
+		cout «(start++)->get() « '\t';
+	}
+	cout « endl;
+	delete[]a;
+	system("pause");
+	return 0;
+}
