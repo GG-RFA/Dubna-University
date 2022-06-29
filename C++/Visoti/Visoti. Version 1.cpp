697d7f3f6c7089b1e1370a82dda9632b54bb707f
Fedor Ronginskiy <108332251+TeddyRFA@users.noreply.github.com>
2022-06-29 11:51:17 +0300

    Add files via upload

---
+++ C++/Visoti/Visoti. Version 1.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+class Visoti {
+private:
+	long int a;
+	long int b;
+public:
+	Visoti() {
+		int a[6] = { 2,4,6,8,10,12 };
+		for (int i = 0; i < 6; i++) {
+			a[i] = 0;
+		}
+	}
+	void max() {
+		int a[6] = { 2,4,6,8,10,12 };
+		int max = a[0];
+		for (int i = 1; i < 6; i++) {
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+	}
+	void min() {
+		int a[6] = { 2,4,6,8,10,12 };
+		int min = a[0];
+		for (int i = 1; i < 6; i++) {
+			if (a[i] < min) {
+				min = a[i];
+			}
+		}
+	}
+	void perepad() {
+		int a[6] = { 2,4,6,8,10,12 };
+		int summa = 0;
+		for (int i = 1; i < 6; i++)
+			summa += a[i];
+	}
+	void krutizna() {
+		int a[6] = { 2,4,6,8,10,12 };
+		double srednee = 0;
+		int summa = 0;
+		for (int i = 1; i < 6; i++)
+			summa += a[i];
+		srednee = summa / i;
+	}
+	void sravnenie() {
+		int a[6] = { 2,4,6,8,10,12 };
+		int b[6] = { 1,3,5,7,9,11 };
+		for (int i = 1; i < 6; i++)
+			for (int j = 1; j < 6; j++)
+
+	}
+};
+int main()
+{
+	Visoti a[6];
+	for (int i = 0; i < 6; i++)
+	{
+
+	}
+	setlocale(LC_ALL, "Russian");
+	system("pause");
+	return 0;
+}
---
+++ C++/Visoti/Visoti. Version 2.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+class Visoti {
+private:
+	int a;
+	int b;
+public:
+	Visoti(int a, int b) {
+		this->a = a;
+		this->b = b;
+	}
+	void maximum() {
+		int a[] = { 2,4,6,8,10,12 };
+		int max = a[0];
+		for (int i = 1; i < 6; i++) {
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+	}
+	void minimum() {
+		int a[] = { 2,4,6,8,10,12 };
+		int min = a[0];
+		for (int i = 1; i < 6; i++) {
+			if (a[i] < min) {
+				min = a[i];
+			}
+		}
+	}
+	void difference() {
+		int a[] = { 2,4,6,8,10,12 };
+		int sum = 0;
+		for (int i = 1; i < 6; i++)
+			sum += a[i];
+	}
+	void krutizna() {
+		int a[] = { 2,4,6,8,10,12 };
+		double average = 0;
+		int sum = 0;
+		for (int i = 1; i < 6; i++)
+			sum += a[i];
+		average = sum / 6;
+	}
+	double comparison() {
+		int a[] = { 2,4,6,8,10,12 };
+		int b[] = { 1,3,5,7,9,11 };
+		for (int i = 1; i < 6; i++)
+			for (int j = 1; j < 6; j++)
+				if (a[i] = b[j]) {
+					return true;
+				}
+				else
+					return false;
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a[6] = { 2,4,6,8,10,12 };
+	int b[6] = { 1,3,5,7,9,11 };
+	Visoti V(a, b);
+	cout « "Максимальное значение:" « V.maximum() « endl;
+	cout « "Минимальное значение:" « V.minimum() « endl;
+	cout « "Сумма элементов:" « V.difference() « endl;
+	cout « "Среднее значение:" « V.krutizna() « endl;
+	cout « "Сравнение:" « V.comparison() « endl;
+	system("pause");
+	return 0;
+}
